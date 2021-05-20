2d043123bf1bf1f614245e6893fed91e600d95d9
chetandasauni <chetandasauni25@gmail.com>
2021-05-20 08:45:28 -0700

    feat: added functionality to create rooms and add user name

--- client/src/app.tsx
+++ client/src/app.tsx
@@ -5,16 +5,60 @@ import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import TextField from '@material-ui/core/TextField';
 import { getSocket, SocketType } from './services/chat';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Box from '@material-ui/core/Box';
+import IconButton from '@material-ui/core/IconButton';
+import Save from '@material-ui/icons/Save';
+
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: any;
+  value: any;
+}
+
+function TabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`scrollable-auto-tabpanel-${index}`}
+      aria-labelledby={`scrollable-auto-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          {children}
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: string) {
+  return {
+    id: `scrollable-auto-tab-${index}`,
+    'aria-controls': `scrollable-auto-tabpanel-${index}`,
+  };
+}
 
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
     root: {
       flexGrow: 1,
+      width: '100%',
       '& .MuiTextField-root': {
         margin: theme.spacing(1),
         width: '25ch',
       },
+    },
+    margin: {
+      margin: theme.spacing(1),
     },
     chatOptions: {
       padding: theme.spacing(2),
@@ -24,25 +68,51 @@ const useStyles = makeStyles((theme: Theme) =>
     },
     chatArea: {
       padding: theme.spacing(1),
+      // border: '1px solid black',
+      height: '100%'
+    },
+    chatTabs: {
+      padding: theme.spacing(1),
       border: '1px solid black',
-      height: '70vh'
+      height: '100%',
+      maxHeight: '55vh',
+      overflowY: 'auto'
     },
     customGrid: {
+        margin: '0px',
         marginTop: '2rem',
-        display: 'flex',
-        justifyContent: 'center'
+        display: 'grid',
+        height: '90vh',
+        gridTemplateRows: 'min-content 2fr min-content',
+        justifyContent: 'center',
+        width: '100%'
     },
     pageOptions: {
-        padding: theme.spacing(2),
-        textAlign: 'center',
-        border: '1px solid black'
+        border: '1px solid black',
+        height: '100%'
     },
     customInput: {
-        width: '100ch !important'
+        width: '80ch !important'
     },
     button: {
         margin: theme.spacing(1),
     },
+    fullWidth: {
+      maxWidth: '100%'
+    },
+    serverStyle: {
+      font: 'bold',
+      color: 'green'
+    },
+    currentUserStyle: {
+      color: 'red',
+      textAlign: 'end'
+    },
+    normalMessageStyle: {
+    },
+    saveIcon: {
+      paddingTop: '1.5rem'
+    }
   }),
 );
 
@@ -54,6 +124,7 @@ type Messages = {
    name?: string;
    message?: string;
    type: string;
+   rooms?: string[];
 }
 
 type User = {
@@ -64,15 +135,16 @@ type User = {
 const App = (): JSX.Element => {
     const classes = useStyles();
     const [messages, setMessages] = useState<Messages[]>([]);
+    const [rooms, setRooms] = useState<string[]>([]);
     const [user, setUser] = useState<User|null>(null);
-    const messageRef = useRef('');
+    const messageRef = useRef<HTMLTextAreaElement|null>(null);
+    const [currentRooom, setCurrentRoom] = useState('Public');
 
     const onReceiveMessage = (messageData: Messages) => {
-        console.log('message', messageData);
-        // const data = JSON.parse(messageData.data);
-        // console.log(data, 'data');
         if (messageData.type === 'Info') {
-            setUser({name: '', userId: messageData.userId});
+            // console.log('message', messageData);
+            setUser({name: 'Test', userId: messageData.userId});
+            setRooms(messageData.rooms || []);
         } else {
             console.log(messageData, 'data-message');
             setMessages((prevState: Messages[]) => {
@@ -106,6 +178,13 @@ const App = (): JSX.Element => {
       }, []);
 
     const sendMessage = () => {
+        console.log('fired');
+         socket$?.next({
+              userId: user?.userId,
+              message: messageRef?.current?.value,
+              name: user?.name,
+              room: currentRooom
+          });
         // ws.send(JSON.stringify({
         //     userId: user.userId,
         //     message: messageRef.current.value,
@@ -113,24 +192,75 @@ const App = (): JSX.Element => {
         // }));
     }
 
+    const getClassNane = (id: number) => {
+        switch(String(id)) {
+          case '1':
+            return classes.serverStyle;
+          case String(user?.userId): 
+            return classes.currentUserStyle;
+          default:
+            return classes.normalMessageStyle;
+        }
+    }
+
+    const handleChange = (event: React.ChangeEvent<{}>, newValue: string) => {
+      setCurrentRoom(newValue);
+    };
+
     return (
         <>
             <CssBaseline />
             <div className={classes.root}>
                 <Grid container spacing={3} className={classes.customGrid}>
-                    <Grid item xs={10}>
-                      <div className={classes.pageOptions}>Options</div>
-                    </Grid>
-                    <Grid item xs={10}>
-                      <div className={classes.chatArea}>
-                            {
-                                messages.map((message, index) => {
-                                  return <p key={index}>{`${message.name} ---- ${message.message}`}</p>
-                                })
-                            }
+                    <Grid item xs={12}>
+                      <div className={classes.pageOptions}>
+                        <form className={classes.root} noValidate autoComplete="off">
+                          <TextField id="standard-basic" label="Display Name" />
+                            <IconButton className={classes.saveIcon} color="primary" aria-label="save display name" component="span">
+                              <Save />
+                            </IconButton>
+                        </form>
                       </div>
                     </Grid>
-                    <Grid item xs={10}>
+                    <Grid item xs={12}>
+                      <div className={classes.chatTabs}>
+                        {
+                          user ?  <AppBar position="static" color="default">
+                            <Tabs
+                              value={currentRooom}
+                              onChange={handleChange}
+                              indicatorColor="primary"
+                              textColor="primary"
+                              variant="scrollable"
+                              scrollButtons="auto"
+                              aria-label="romms avialbale"
+                            >
+                              {
+                                  rooms.map((roomName, index) => {
+                                    return <Tab value={roomName} label={roomName} key={index} {...a11yProps(roomName)} />
+                                  })
+                              }
+                            </Tabs>
+                          </AppBar> : null
+                        } 
+                        {
+                            messages ? rooms.map((roomName, index) => {
+                              return <TabPanel value={roomName} index={roomName} key={index}>
+                                <div className={classes.chatArea}>
+                                      {
+                                          messages.map((message, index) => {
+                                            return <p key={index} className={getClassNane(message.userId)}>
+                                              {`${message.userId}:  ${message.message}`}
+                                            </p>
+                                          })
+                                      }
+                                </div>
+                              </TabPanel>
+                            }) : null
+                        }
+                      </div>
+                    </Grid>
+                    <Grid item xs={12}>
                         <div className={classes.chatOptions}>
                            <TextField
                                 id="outlined-multiline-static"
--- server/chat.ts
+++ server/chat.ts
@@ -4,12 +4,27 @@ import {
   } from 'https://deno.land/std/ws/mod.ts'
   import { v4 } from 'https://deno.land/std/uuid/mod.ts'
   
-  const users = new Map<string, WebSocket>()
+  const users = new Map<string, WebSocket>();
+  const rooms: Record<string, Record<string, WebSocket>> = {};
   
   function broadcast(message: string, senderId?: string): void {
     if (!message) return
     for (const user of users.values()) {
       user.send(senderId ? `[${senderId}]: ${message}` : message)
+    }
+  }
+
+  function broadcastToChannel(message: string, senderId: string, channel: string, senderName?: string): void {
+    if (!message) return
+    const channelUsers = rooms[channel];
+    for (const [userId, socket] of Object.entries(channelUsers)) {
+      socket.send(JSON.stringify({
+        type: 'Message',
+        name: senderName,
+        message: message,
+        userId: senderId
+    }));
+      // user.send(senderId ? `[${senderId}]: ${message}` : message)
     }
   }
   
@@ -18,19 +33,26 @@ import {
     const userId = v4.generate()
   
     // Register user connection
-    users.set(userId, ws)
-    broadcast(JSON.stringify({
-        type: 'Message',
-        name: 'Server',
-        message: 'Welcome User',
-        userId: '1'
-    }));
+    users.set(userId, ws);
+    // join user to the public group
+    if (rooms['Public']) {
+      rooms['Public'][userId] = ws;
+    } else {
+      rooms['Public'] = {};
+      rooms['Public'][userId] = ws;
+    }
+
+    broadcastToChannel(`User ${userId} has jooined`, '1', 'Public', 'Server');
+    
+    // send message to joined user
+    const availableGroups = Object.keys(rooms);
     const user = users.get(userId);
     if (user) {
         user.send(JSON.stringify({
             type: 'Info',
             message: 'Welcome User your Chat Info',
-            userId: userId
+            userId: userId,
+            rooms: availableGroups
         }));
     }
 
@@ -39,13 +61,20 @@ import {
       const message = typeof event === 'string' ? event : ''
       console.log(message, userId, 'incoming');
       // const messageData = JSON.parse(message);
-    //   broadcast(message, userId);
-       broadcast(message);
+      //   broadcast(message, userId);
       // Unregister user conection
       if (!message && isWebSocketCloseEvent(event)) {
+        console.log('disconnected');
+        const { code, reason } = event;
+        console.log("ws:Close", code, reason);
         users.delete(userId)
-        broadcast(`> User with the id ${userId} is disconnected`)
+        delete rooms['Public'][userId];
+        broadcastToChannel(`User ${userId} has left`, '1', 'Public', 'Server');
+        // broadcast(`> User with the id ${userId} is disconnected`)
         break;
+      } else {
+        const messageData = JSON.parse(message);
+        broadcastToChannel(messageData.message, messageData.userId, messageData.room, messageData.name);
       }
     }
   }
